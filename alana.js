2f411419676e30922bf34af10195c64c61b43a0e
Anjo-dev01 <lucasgsilva.dev@gmail.com>
2026-05-01 20:19:41 -0300

    Delete alana.js

--- alana.js
+++
@@ -1,9 +0,0 @@
-const envelope = document.getElementById("envelope");
-const musica = document.getElementById("musica");
-
-envelope.addEventListener("click", () => {
-  envelope.classList.toggle("open");
-
-  // tocar música
-  musica.play();
-});
